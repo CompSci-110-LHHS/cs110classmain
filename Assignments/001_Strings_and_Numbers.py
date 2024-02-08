eb4d1bc6f92b20074fef9ae8714adb2519627c9d
Jeff McDowell <mcdowell.jeffrey123@gmail.com>
2024-02-08 09:15:55 -0400

    Added first assignment to class repo

---
+++ Assignments/001_Strings_and_Numbers.py
@@ -0,0 +1,59 @@
+############################################################
+########### UNIT 1: Python Basics Assessment ###############
+######## Practice With Variables: Numbers and Strings ######
+############################################################
+
+########## SECTION: Numbers and Math Operators #############
+
+################### QUESTION 1 ############################
+#Calculate the Area (A) and circumference (C) of a circle if r = 7 
+#and C = 2*pi*r and A = pi * r**2. print() to the screen
+
+#
+# Write Your Code Here
+#
+
+##################### QUESTION 2 ############################
+#Calculate the Area (A) and perimeter (P) of a rectangle if
+#A = b * h and P = sideA + sideB + sideC + sideD. print() to the screen
+
+#
+# Write Your Code Here
+#
+
+#################### QUESTION 3 ############################
+#Define two types of numbers and use a python method to return the
+#'type' of variable.  Then use type casting to change one variable
+#'type' to another (i.e. Change a int to float) 
+
+#
+# Write Your Code Here
+#
+
+################# SECTION: Strings #######################
+
+################### QUESTION 4 ############################
+#Declare and assign at least three variables and write a decriptive
+#sentence about yourself (i.e.hobbies, pet names etc).
+
+#Using f-strings: 
+#
+# Write Your Code Here
+#
+
+#Using format():
+#
+# Write Your Code Here
+#
+
+################### QUESTION 5 ############################
+#Take the string below and parse out the information required.
+
+lhhsAdd = "Leo Hayes High School - 499 Clife Street - E3A 9P5"
+
+#schoolName
+#schoolStreet
+#schoolPostCode
+
+#Use f-strings to write a sentence that reads:
+#"Leo Hayes High School is located on 499 Clife Street"
