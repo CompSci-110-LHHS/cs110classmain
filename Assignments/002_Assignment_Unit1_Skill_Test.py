10d40934ffa80895d449e15f1e34a9aefeb69c2a
Jeff McDowell <mcdowell.jeffrey123@gmail.com>
2024-02-14 11:47:47 -0400

    fixed assignment 02 typo

--- Assignments/002_Assignment_Unit1_Skill_Test.py
+++ Assignments/002_Assignment_Unit1_Skill_Test.py
@@ -5,10 +5,24 @@
 #ex. Jeffrey Jared McDowell --> McDowell, Jeffrey J
 
 ###########################  Question 2 ########################### 
-#(PART 1) Take the string below and split the values on the comma and store the first name and
+#(PART 1) Take the string below and split the values on the comma and store the first name, last_name, age, grade
 # in a dictionary of dictionaries (HINT: use the student_ID as the key for the outermost dictionary)
 
 #This string stores: first_name, last_name, age, grade, student_ID
+
+studentDatabase = {
+    "1001224":{
+        "first_name":"Adam",
+        },
+    "1005644":{"first_name":"Cassie"},
+    "120534":{"first_name":"Hailey"}
+}
+
+print(studentDatabase["120534"]["first_name"])
+
+
+
+
 student1 = "Adam,Day, 18, 4, 1001224"
 student2 = "Cassie, Stevens, 16, 1, 1005644"
 student3 = "Hailey, Powers, 17, 4, 120534"
